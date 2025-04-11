278cab17590a5328ffbf4c5d8bd070d3eeae631a
Juan Carlos <taraguipora2005@gmail.com>
2025-04-11 19:40:06 -0300

    Refactor app component and module to integrate DBZ features, including main page and agregar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ContadorComponent } from './contador/contador/contador.component';
-import { HeroeComponent } from './Heroes/Heroe/Heroe.component';
-import { ListadoComponent } from "./Heroes/listado/listado.component";
+import { MainPageComponent } from './dbz/main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [ContadorComponent]
+ imports: [MainPageComponent]
 })
 export class AppComponent {
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 
 import { HeroeComponent } from './Heroes/Heroe/Heroe.component';
 import { ListadoComponent } from './Heroes/listado/listado.component';
 import { HeroesModule } from './Heroes/heroes.modulo';
 import { ContadorModule } from './contador/ContadorModule';
+import { DbzModule } from './dbz/dbz.module';
+import { AgregarComponent } from './dbz/agregar/agregar.component';
+import { DbzService } from './dbz/Services/dbz.services';
 
 @NgModule({
     declarations: [
     ],
     imports: [
+        AgregarComponent,
+        FormsModule,
+        DbzModule,
         BrowserModule,
         ContadorModule,
         HeroeComponent,
@@ -18,7 +26,7 @@ import { ContadorModule } from './contador/ContadorModule';
         AppComponent,
         HeroesModule,
     ],
-    providers: [],
+
     // Removed bootstrap array as AppComponent is standalone
     })
     export class AppModule { }
